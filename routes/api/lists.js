ff6f1e64f85b678139d985fcb0010092259970ab
Kyle Wright <kylejamwright@gmail.com>
2020-08-20 19:04:20 -0600

    Implemented Read lists in the service layer

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const jwt = require('jsonwebtoken');
 var router = express.Router();
 const {
     readLists
@@ -6,9 +7,16 @@ const {
 
 // GET a user's lists
 router.get('/:id', async (req, res, next) => {
-    const id = req.params.id;
-    const data = await readLists(id);
-    res.send(data);
+    const token = req.header('auth');
+
+    if (token) {
+        const decoded = await jwt.verify(token, process.env.JWT_KEY);
+        const data = await readLists(decoded.id);
+        res.send(data);
+    } else {
+        res.status(403).send();
+    }
+    
 });
 
 module.exports = router;
