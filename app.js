0660915492b900afd2aca775cff15b81619e4233
Kyle Wright <kylejamwright@gmail.com>
2020-08-22 14:00:21 -0600

    Initial deployment to Heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/lists', listsRouter);
 
---
+++ public/precache-manifest.5c421df7817cdb9d189bddf6e54c8064.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139670aaadbffa2c47a3b67b5e38ab1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cc480c30f46333a4332",
+    "url": "/static/css/main.17fee3fd.chunk.css"
+  },
+  {
+    "revision": "8ae8b3a87c34a78cc10b",
+    "url": "/static/js/2.6dc51f82.chunk.js"
+  },
+  {
+    "revision": "ef20b9ddd7cf7cf5039dc421fe655af9",
+    "url": "/static/js/2.6dc51f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc480c30f46333a4332",
+    "url": "/static/js/main.6967c6a8.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  },
+  {
+    "revision": "7cfaf174ddb219a3c70ebf0f668f96da",
+    "url": "/static/media/bgImg.7cfaf174.jpg"
+  },
+  {
+    "revision": "7a95487b36327bc106c7270198fcce3d",
+    "url": "/static/media/bgImgMobile.7a95487b.jpg"
+  },
+  {
+    "revision": "cddeebc3f589c9a978b22f8b2a518d2f",
+    "url": "/static/media/logo.cddeebc3.png"
+  }
+]);
